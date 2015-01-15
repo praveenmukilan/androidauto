3ed779c028d8e50f2b8eea5dd5bd7870c205f857
praveenmukilan <praveen.m@mig.me>
2015-01-15 16:20:18 +0800

    clear

--- autoweb/src/com/autome/web/TestSuite.java
+++ autoweb/src/com/autome/web/TestSuite.java
@@ -1,5 +1,57 @@
 package com.autome.web;
 
-public class TestSuite {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestSuite {
+	
+	public static WebDriver driver;
+	public static Properties webprop;
+	@BeforeClass
+	public static void init(){
+		
+		try{
+		System.setProperty("webdriver.chrome.driver", "src//resources//chromedriver");
+
+		   driver = new ChromeDriver();
+		   webprop= new Properties();
+		   FileInputStream webFis = new FileInputStream("//src//resources//OR.properties");
+           webprop.load(webFis);
+		}catch(FileNotFoundException e){
+			
+			System.out.println(e.getMessage());
+		}catch(IOException e){
+			
+			System.out.println(e.getMessage());
+		}
+	
+	}
+	
+	@Test
+	public static void runTest(){
+		
+		driver.get(webprop.getProperty("URL"));
+		
+		driver.findElement(By.cssSelector(webprop.getProperty("lnk_SignIn")));
+		
+		driver.findElement(By.cssSelector(webprop.getProperty("txt_UserName")));
+		driver.findElement(By.cssSelector(webprop.getProperty("txt_Password")));
+		
+		driver.findElement(By.cssSelector(webprop.getProperty("lnk_SignIn")));
+		
+		driver.findElement(By.cssSelector(webprop.getProperty("lnk_SignIn")));
+		
+		
+		
+	}
+	
+	
 }
